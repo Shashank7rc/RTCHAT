6f761d287949ef04aee520438eb3bedb38a9d247
Shashank Singh <shashank.rajput2k17@gmail.com>
2026-06-19 02:23:26 +0530

    created message routes+basic frontend

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,23 @@
 import express from "express";
+import { arcjetprtotection } from "../middleware/arcjet.middleware.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getAllContacts,
+        getChatPartners,
+        sendMessage, 
+        getMessagesByUserId} from "../controllers/message.controller.js";
+
+
 
 const router = express.Router();
 
-router.get('/message',(req,res)=>{
+router.use(arcjetprtotection,protectRoute);
 
-})
+router.get("/contacts",getAllContacts); /*protectRoute adds user to req body then 
+in getAllContacts we can access it   const loggedInUserId = req.user._id; so protectRoute middleware
+is required*/
 
+router.get("/chats", getChatPartners);
+router.get("/:id", getMessagesByUserId);
+router.post("/send/:id", sendMessage);
 
 export default router;
